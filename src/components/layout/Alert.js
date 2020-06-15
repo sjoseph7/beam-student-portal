205b307e6bb8997dc798f2fb499a435433c3d140
sjoseph7 <sjoseph7.git@gmail.com>
2020-06-14 19:04:37 -0700

    refactor: clean up code

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -3,11 +3,11 @@ import AlertContext from "../../context/alert/alertContext";
 
 //TODO: Refactor with Container
 const Alert = () => {
-  const alertContext = useContext(AlertContext);
+  const { alerts } = useContext(AlertContext);
   return (
-    alertContext.alerts.length > 0 &&
-    alertContext.alerts.map(alert => (
-      <div key={alert.id} className={`alert alert-${alert.type}`}>
+    alerts.length > 0 &&
+    alerts.map(alert => (
+      <div key={alert.id} className={`m-0 alert alert-${alert.type}`}>
         {alert.message}
       </div>
     ))
